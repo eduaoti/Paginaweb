1c28efc0ca43dabe53f3835dc145a89927b4cbb2
eduaoti <eduardo4t8@gmail.com>
2025-06-13 19:35:36 -0600

    Agregar workflow de despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Paginaweb/', // 👈 Muy importante: coincide con el nombre del repo en GitHub
 })
+
