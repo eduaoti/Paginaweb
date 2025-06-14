c25cffe29ae4077717e61aeb094c29a9f088b2c0
eduaoti <eduardo4t8@gmail.com>
2025-06-13 19:08:20 -0600

    Primer commit del proyecto React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Página de Edu</h1>
+      <h2 className="subtitle">Eduardo González Ortiz</h2>
+    </div>
+  );
+}
+
+export default App;
