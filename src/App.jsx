cab100b2fb3a15d81cdba17f0b4e3974cd6e0cd6
Eduardo <135056458+eduaoti@users.noreply.github.com>
2025-06-13 20:38:30 -0600

    Update App.jsx
    Hola

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function Home() {
       <h2 className="subtitle">Eduardo González Ortiz</h2>
       <h2 className="subtitle">ME gustan los perros</h2>
       <h2 className="subtitle">Más no los gatos</h2>
+      <h2 className="subtitle">En fin gustos de cada quien</h2>
     </div>
   );
 }
