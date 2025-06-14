f597df01a356794b20d2e8e0edfe4ded79f4f984
Eduardo <135056458+eduaoti@users.noreply.github.com>
2025-06-13 19:52:16 -0600

    Update App.jsx
    No los gatos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
     <div className="container">
       <h1 className="title">Página de Edu</h1>
       <h2 className="subtitle">Eduardo González Ortiz</h2>
-        <h2 className="subtitle">ME gustan los perros</h2>
+      <h2 className="subtitle">ME gustan los perros</h2>
+      <h1 className="subtitle">Más no los gatos</h1>
     </div>
   );
 }
